0226b083f4f52ecc3e3cfcc9d52d8e3fb638143e
Gareth Hughes <gurrkin@googlemail.com>
2015-04-19 12:34:09 +0100

    [Update] Getting to grips with Firebase again (and failing mostly)

--- src/javascripts/firebase/fireblogger.firebase.js
+++ src/javascripts/firebase/fireblogger.firebase.js
@@ -1,11 +1,11 @@
 (function () {
-	'use strict';
+	'use strict'
 
 	angular.module('fireblogger.firebase', [])
 
 		.value('fb', new Firebase('https://fiery-heat-3490.firebaseio.com/').child('blogEntries'))
 
-		.factory('FirebaseFactory', ['fb', function (fb) {
+		.factory('FirebaseFactory', ['fb', '$timeout', function (fb, $timeout) {
 			var model = {
 				posts: []
 			}
@@ -13,13 +13,13 @@
 			return {
 				fbModel: model,
 
-				loadAllPosts: function () {
-					fb.on('value', function (snapshot){
-						var posts = snapshot.val()
-						for (var i = 0; i < posts.length; i++) {
-							model.posts.unshift(posts[i])
-						}
-					});
+				loadAllPosts: function (callback) {
+					fb.on('child_added', function (snapshot){
+						$timeout(function () {
+							model.posts.unshift(snapshot.val())
+							callback()
+						})
+					})
 				}
 			}
 		}])
--- src/javascripts/firebase/fireblogger.firebase.spec.js
+++ src/javascripts/firebase/fireblogger.firebase.spec.js
@@ -4,33 +4,33 @@ var snapshot = {
 		{ 'post-2': {'title': 'test post 2'} }
 	],
 	val: function () {
-		return this.posts;
+		return this.posts
 	}
 }
 
 describe('factories', function () {
 	describe('FirebaseFactory', function () {
-		var FirebaseFactory, fb;
+		var FirebaseFactory, fb
 
 		beforeEach(function () {
-			module('fireblogger.firebase');
+			module('fireblogger.firebase')
 
 			inject(function (_FirebaseFactory_, _fb_) {
 				FirebaseFactory = _FirebaseFactory_
-				fb = _fb_;
+				fb = _fb_
 			})
 		})
 
 		it('should be initialised with a model', function () {
-			FirebaseFactory.fbModel.posts.should.be.empty;
+			FirebaseFactory.fbModel.posts.should.be.empty
 		})
 
 		it('should load all of the posts', function () {
-			sinon.stub(fb, 'on').yields(snapshot);
+			sinon.stub(fb, 'on').yields(snapshot)
 
-			FirebaseFactory.loadAllPosts();
-
-			FirebaseFactory.fbModel.posts.length.should.equal(2);
+			FirebaseFactory.loadAllPosts(function () {
+				FirebaseFactory.fbModel.posts.length.should.equal(2)
+			})
 		})
 	})
 })
