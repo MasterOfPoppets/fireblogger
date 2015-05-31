e57fc87a0c7cda93894c013452b2b1129a5da9e9
Gareth Hughes <gurrkin@googlemail.com>
2015-05-31 16:37:11 +0100

    [Update] Set with priority - actually setting it

--- src/javascripts/firebase/firebase.js
+++ src/javascripts/firebase/firebase.js
@@ -22,7 +22,7 @@
 				},
 
 				savePost: function (postObj) {
-					fb.child(postObj.url).setWithPriority(postObj)
+					fb.child(postObj.url).setWithPriority(postObj, Firebase.ServerValue.TIMESTAMP)
 				}
 			}
 		}])
